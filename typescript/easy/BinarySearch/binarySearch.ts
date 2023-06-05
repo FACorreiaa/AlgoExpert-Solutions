71730b1c693ebbcf5ad71a1b75d0657064267417
fc <fc@gsmk.de>
2023-06-05 22:19:32 +0100

    change project structure

---
+++ typescript/easy/BinarySearch/binarySearch.ts
@@ -0,0 +1,19 @@
+// Time O(logN) | Space O(1)
+
+function interactiveBinarySearch(array: Array<number>, target: number): number {
+	let left = 0;
+	let right: number = array.at(-1) as number;
+	//let right = array.length - 1;
+	while (left < right) {
+		let middle = (left + 2) / 2;
+		let potentialMatch = array[middle];
+		if (target == potentialMatch) {
+			return middle;
+		} else if (target < potentialMatch) {
+			right = middle - 1;
+		} else {
+			left = middle + 1;
+		}
+	}
+	return -1;
+}
